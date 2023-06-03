a05e86229f7a3cdfb398016b79281810fe26cacb
14837-eng <mario2013@inbox.ru>
2023-06-03 20:09:21 +0300

    feat(book): Add listenBooks method

--- src/app/core/services/books.service.ts
+++ src/app/core/services/books.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import booksJson from 'src/assets/simulate/db/books.json';
+import { IAuthor } from '../interfaces/author.interface';
+import { IBook } from '../interfaces/book.interface';
+import { AuthorsService } from './authors.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BooksService {
-  private books$: BehaviorSubject<any> = new BehaviorSubject([]);
+  private books$: BehaviorSubject<IBook[]> = new BehaviorSubject([] as IBook[]);
 
-  constructor() {
-    const books = booksJson['books'];
+  constructor(private authorsService: AuthorsService) {
+    const books: any = booksJson['books'];
     this.books$.next(books);
+  }
+
+  setDataOfAuthorsOfBooks = (books: IBook[]) => {
+    return books.map((book) => {
+      if (!book.authors.length) return book;
+      book.authors = book.authors.map((author: IAuthor) => {
+        const findedAuthor = this.authorsService.getAuthorByID(author.id);
+        return findedAuthor !== undefined ? findedAuthor : author;
+      });
+      return book;
+    });
+  };
+
+  listenBooks() {
+    return this.books$.asObservable().pipe(map(this.setDataOfAuthorsOfBooks));
   }
 }
