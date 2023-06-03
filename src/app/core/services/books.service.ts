7b7b5606ea318edacd981e0a776c95250b87bcd4
14837-eng <mario2013@inbox.ru>
2023-06-03 16:02:46 +0300

    feat(books): Add service for books

--- src/app/core/services/books.service.ts
+++ src/app/core/services/books.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import booksJson from 'src/assets/simulate/db/books.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BooksService {
+  private books$: BehaviorSubject<any> = new BehaviorSubject([]);
 
-  constructor() { }
+  constructor() {
+    const books = booksJson['books'];
+    this.books$.next(books);
+  }
 }
