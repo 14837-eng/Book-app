64f53aed5e9b3a60d6061cbcb3389bbb69395e8c
14837-eng <mario2013@inbox.ru>
2023-06-03 21:11:27 +0300

    feat(app): Set title app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-test-pr';
+export class AppComponent implements OnInit {
+  constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    const title = environment.appName;
+    this.titleService.setTitle(title);
+  }
 }
