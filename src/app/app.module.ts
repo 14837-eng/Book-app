56f7c6290c69fdedc62b975db7ba8c25f662eba6
14837-eng <mario2013@inbox.ru>
2023-06-03 16:03:03 +0300

    feat(books): Enable service in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksService } from './core/services/books.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [BooksService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
