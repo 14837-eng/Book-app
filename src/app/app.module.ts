ebf452688d4545de2395cf4314429f1dd5f9b206
14837-eng <mario2013@inbox.ru>
2023-06-03 17:07:31 +0300

    Revert "feat(books): Enable service in app"
    This reverts commit 56f7c6290c69fdedc62b975db7ba8c25f662eba6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksService } from './core/services/books.service';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [BooksService],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
