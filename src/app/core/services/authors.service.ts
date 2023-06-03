ae6682cdd7266e876ac95fc69c7bddb7e515fb93
14837-eng <mario2013@inbox.ru>
2023-06-03 17:45:24 +0300

    feat(author): Edit interfaces in author service

--- src/app/core/services/authors.service.ts
+++ src/app/core/services/authors.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import authorsJson from 'src/assets/simulate/db/authors.json';
+import { IAuthor } from '../interfaces/author.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorsService {
-  private authors$: BehaviorSubject<any> = new BehaviorSubject([]);
+  private authors$: BehaviorSubject<IAuthor[]> = new BehaviorSubject(
+    [] as IAuthor[]
+  );
 
   constructor() {
     const authors = authorsJson['authors'];
