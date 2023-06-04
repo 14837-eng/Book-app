95df7b0fac94123d1a56df5c593fb48c7d14688d
14837-eng <mario2013@inbox.ru>
2023-06-04 15:50:06 +0300

    feat(books): Add search by title

--- src/app/core/services/books.service.ts
+++ src/app/core/services/books.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, map } from 'rxjs';
 import booksJson from 'src/assets/simulate/db/books.json';
 import { IAuthor } from '../interfaces/author.interface';
 import { IBook } from '../interfaces/book.interface';
+import { BookFilter } from '../models/books.model';
 import { AuthorsService } from './authors.service';
 
 @Injectable({
@@ -10,6 +11,7 @@ import { AuthorsService } from './authors.service';
 })
 export class BooksService {
   private books$: BehaviorSubject<IBook[]> = new BehaviorSubject([] as IBook[]);
+  private filters = new BookFilter();
 
   constructor(private authorsService: AuthorsService) {}
 
@@ -28,8 +30,25 @@ export class BooksService {
     return this.books$.asObservable().pipe(map(this.setDataOfAuthorsOfBooks));
   }
 
+  setSearch(value: string) {
+    this.filters.search = value;
+  }
+  private searchByTitle(books: IBook[], value: string) {
+    return books.filter((book) => {
+      const searchValue = value.toLowerCase();
+      const title = book.title.toLowerCase();
+      return title.includes(searchValue);
+    });
+  }
+
   getBooks() {
-    const books: any = booksJson['books'];
+    const { search } = this.filters;
+    let books: any = booksJson['books'];
+
+    if (search) {
+      books = this.searchByTitle(books, search);
+    }
+
     this.books$.next(books);
   }
 }
