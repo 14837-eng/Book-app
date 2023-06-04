d766c49b6b380c61e5edc414ed756d1dcbe22dde
14837-eng <mario2013@inbox.ru>
2023-06-04 19:02:06 +0300

    feat(book-item): Add action btns

--- src/app/shared/components/book-item/book-item.component.ts
+++ src/app/shared/components/book-item/book-item.component.ts
@@ -1,10 +1,17 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
   Input,
   OnInit,
+  Output,
 } from '@angular/core';
 import { IBook } from 'src/app/core/interfaces/book.interface';
+
+export interface ActionButtonClickPayload {
+  book: IBook;
+  action: string;
+}
 
 @Component({
   selector: 'app-book-item',
@@ -16,7 +23,18 @@ export class BookItemComponent implements OnInit {
   @Input() book!: IBook;
   @Input() mini = false;
 
+  @Input() actionBtns: string[] = [];
+  @Output() onActionBtnClick: EventEmitter<ActionButtonClickPayload> =
+    new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClick(action: string) {
+    this.onActionBtnClick.next({
+      book: this.book,
+      action,
+    });
+  }
 }
