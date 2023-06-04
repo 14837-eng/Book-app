a95b83921729538a94be451ebac4bf25c5fb7c97
14837-eng <mario2013@inbox.ru>
2023-06-04 12:20:07 +0300

    feat(main-page): Add books in main page

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './main-page.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [MainPageComponent],
-  imports: [CommonModule, MainPageRoutingModule],
+  declarations: [MainPageComponent, BooksListComponent],
+  imports: [CommonModule, MainPageRoutingModule, SharedModule],
 })
 export class MainPageModule {}
