e78af9895e9990cef98d923e239d06b4a0d183d6
14837-eng <mario2013@inbox.ru>
2023-06-04 19:48:33 +0300

    feat(books-page): Add content

--- src/app/book-page/book-page.component.ts
+++ src/app/book-page/book-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, ReplaySubject, take, takeUntil } from 'rxjs';
+import { IBook } from '../core/interfaces/book.interface';
+import { BooksService } from '../core/services/books.service';
 
 @Component({
   selector: 'app-book-page',
   templateUrl: './book-page.component.html',
-  styleUrls: ['./book-page.component.scss']
+  styleUrls: ['./book-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BookPageComponent implements OnInit {
+export class BookPageComponent implements OnInit, OnDestroy {
+  destroy$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor() { }
+  book$!: Observable<IBook>;
+
+  constructor(
+    private booksService: BooksService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe((params) => {
+        const { id } = params;
+        this.book$ = this.booksService.getBookByID(Number(id));
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete;
+  }
 }
--- src/app/book-page/book-page.module.ts
+++ src/app/book-page/book-page.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookPageComponent } from './book-page.component';
 import { BookPageRoutingModule } from './book-page-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [BookPageComponent],
-  imports: [CommonModule, BookPageRoutingModule],
+  imports: [CommonModule, BookPageRoutingModule, MatButtonModule, SharedModule],
 })
 export class BookPageModule {}
