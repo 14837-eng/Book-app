793d17ea9b56fd279d3dae7b81901243e698a2cf
14837-eng <mario2013@inbox.ru>
2023-06-04 14:56:57 +0300

    feat(book-item): Add cd onpush

--- src/app/shared/components/book-item/book-item.component.ts
+++ src/app/shared/components/book-item/book-item.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { IBook } from 'src/app/core/interfaces/book.interface';
 
 @Component({
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
   styleUrls: ['./book-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookItemComponent implements OnInit {
   @Input() book!: IBook;
