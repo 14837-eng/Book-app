0ffe62f9116377e3b5c098eba677d591593d511e
14837-eng <mario2013@inbox.ru>
2023-06-05 14:28:05 +0300

    feat(books): Enable creating books

--- src/app/book-create-page/book-create-page.component.ts
+++ src/app/book-create-page/book-create-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Book } from '../core/models/book.model';
+import { BooksService } from '../core/services/books.service';
+import { BookFormPayload } from '../shared/components/book-form/book-form.component';
 
 @Component({
   selector: 'app-book-create-page',
   templateUrl: './book-create-page.component.html',
-  styleUrls: ['./book-create-page.component.scss']
+  styleUrls: ['./book-create-page.component.scss'],
 })
 export class BookCreatePageComponent implements OnInit {
+  constructor(private booksService: BooksService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  saveBook(event: BookFormPayload) {
+    this.booksService.createBook(
+      new Book({
+        title: event.title,
+        subtitle: event.subtitle,
+        languages: [event.language.name],
+        authors: [event.author],
+        bookshelves: [event.genre],
+        number_of_pages: event.count_of_page,
+      })
+    );
+    this.router.navigate(['/']);
   }
-
 }
