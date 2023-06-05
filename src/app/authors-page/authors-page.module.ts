27e000e84a2ccfe436e44377de23d88b79fb6bb0
14837-eng <mario2013@inbox.ru>
2023-06-05 17:03:18 +0300

    feat(author-page): Change view

--- src/app/authors-page/authors-page.module.ts
+++ src/app/authors-page/authors-page.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorsPageComponent } from './authors-page.component';
 import { AuthorsPageRoutingModule } from './authors-page-routing.module';
+import { AuthorsListComponent } from './authors-list/authors-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AuthorsPageComponent],
-  imports: [CommonModule, AuthorsPageRoutingModule],
+  declarations: [AuthorsPageComponent, AuthorsListComponent],
+  imports: [
+    CommonModule,
+    AuthorsPageRoutingModule,
+    SharedModule,
+    MatButtonModule,
+  ],
 })
 export class AuthorsPageModule {}
