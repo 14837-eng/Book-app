3c432a247c0de5d4b1c664e37b37971a0a94d19e
14837-eng <mario2013@inbox.ru>
2023-06-05 15:51:58 +0300

    feat(authors-page): Add cd onpush

--- src/app/authors-page/authors-page.component.ts
+++ src/app/authors-page/authors-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-authors-page',
   templateUrl: './authors-page.component.html',
-  styleUrls: ['./authors-page.component.scss']
+  styleUrls: ['./authors-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthorsPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
