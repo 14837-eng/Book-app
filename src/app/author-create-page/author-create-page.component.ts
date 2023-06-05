68144f911ba0b0ae7cce99e15252e701fc83cb96
14837-eng <mario2013@inbox.ru>
2023-06-05 18:03:09 +0300

    feat(author-create) Add saving data

--- src/app/author-create-page/author-create-page.component.ts
+++ src/app/author-create-page/author-create-page.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Author } from '../core/models/author.model';
+import { AuthorsService } from '../core/services/authors.service';
+import { AuthorFormPayload } from '../shared/components/author-form/author-form.component';
 
 @Component({
   selector: 'app-author-create-page',
@@ -7,7 +12,27 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthorCreatePageComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private authorsService: AuthorsService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
+
+  saveAuthor(event: AuthorFormPayload) {
+    this.authorsService.createAuthor(
+      new Author({
+        name: event.name,
+        birth_year: event.birth_year,
+        death_year: event.death_year,
+      })
+    );
+    this.snackBar.open('success', 'Ok', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      duration: 3000,
+    });
+    this.router.navigate(['/authors']);
+  }
 }
--- src/app/author-create-page/author-create-page.module.ts
+++ src/app/author-create-page/author-create-page.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorCreatePageComponent } from './author-create-page.component';
 import { AuthorCreatePageRoutingModule } from './author-create-page-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [AuthorCreatePageComponent],
-  imports: [CommonModule, AuthorCreatePageRoutingModule],
+  imports: [
+    CommonModule,
+    AuthorCreatePageRoutingModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    SharedModule,
+  ],
 })
 export class AuthorCreatePageModule {}
