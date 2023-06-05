56846bcb43dbb20e4bdc929f28a84c64c014b58b
14837-eng <mario2013@inbox.ru>
2023-06-05 18:11:25 +0300

    feat(author-page): Add edit

--- src/app/author-page/author-page.component.ts
+++ src/app/author-page/author-page.component.ts
@@ -1,4 +1,11 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, ReplaySubject, take, takeUntil } from 'rxjs';
+import { IAuthor } from '../core/interfaces/author.interface';
+import { Author } from '../core/models/author.model';
+import { AuthorsService } from '../core/services/authors.service';
+import { AuthorFormPayload } from '../shared/components/author-form/author-form.component';
 
 @Component({
   selector: 'app-author-page',
@@ -7,7 +14,56 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthorPageComponent implements OnInit {
-  constructor() {}
+  destroy$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  ngOnInit(): void {}
+  author$!: Observable<IAuthor>;
+
+  isEdit = false;
+
+  constructor(
+    private authorsService: AuthorsService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe((params) => {
+        const { isEdit } = params;
+        this.isEdit = isEdit;
+      });
+
+    this.route.params
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe((params) => {
+        const { id } = params;
+        this.author$ = this.authorsService.getAuthorByID(Number(id));
+      });
+  }
+
+  editAuthor(event: AuthorFormPayload, author: IAuthor) {
+    if (author.id === undefined) return;
+    this.authorsService.editAuthorByID(
+      author.id,
+      new Author({
+        ...author,
+        name: event.name,
+        birth_year: event.birth_year,
+        death_year: event.death_year,
+      })
+    );
+    this.snackBar.open('success', 'Ok', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      duration: 3000,
+    });
+    this.router.navigate(['/authors']);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete;
+  }
 }
--- src/app/author-page/author-page.module.ts
+++ src/app/author-page/author-page.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorPageComponent } from './author-page.component';
 import { AuthorPageRoutingModule } from './author-page-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AuthorPageComponent],
-  imports: [CommonModule, AuthorPageRoutingModule],
+  imports: [
+    CommonModule,
+    AuthorPageRoutingModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    SharedModule,
+  ],
 })
 export class AuthorPageModule {}
