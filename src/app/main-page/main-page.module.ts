338a761da9b4245a65243d77b8241c2c8bc34820
14837-eng <mario2013@inbox.ru>
2023-06-05 10:08:47 +0300

    feat(main-page): Add navigate to create

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -9,6 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [MainPageComponent, BooksListComponent, FilterPanelComponent],
@@ -19,6 +20,7 @@ import { MatInputModule } from '@angular/material/input';
     SharedModule,
     MatFormFieldModule,
     MatInputModule,
+    MatButtonModule,
     MatSelectModule,
   ],
 })
